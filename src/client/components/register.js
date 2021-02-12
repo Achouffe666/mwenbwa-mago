6733bb059874fe25a7bf9f8b458bb867c418c073
Marmouz82 <gtn.massart@gmail.com>
2021-02-12 17:10:17 +0100

    add submit button

--- src/client/components/register.js
+++ src/client/components/register.js
@@ -50,6 +50,7 @@ const Register = () => (
                 placeholder={"Birthday"}
                 required
             />
+            <button type={"submit"}>{"Submit"}</button>
         </form>
     </div>
 );
