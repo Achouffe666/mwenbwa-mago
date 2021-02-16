7a080b8f1e0d7bc14c52b8f1678afc20a39976e3
Maud-Pixel <leleuxmaud@gmail.com>
2021-02-16 19:52:31 +0100

    carte ok

--- src/client/components/map.js
+++ src/client/components/map.js
@@ -1,59 +1,51 @@
-import React, {Component} from "react";
-import {
-    MapContainer,
-    TileLayer,
-    GeoJSON,
-    Marker,
-    FeatureGroup,
-} from "react-leaflet";
+import React, {useRef} from "react";
+import {Marker, MapContainer, TileLayer, FeatureGroup} from "react-leaflet";
 //import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 import "react-leaflet-markercluster/dist/styles.min.css";
-import geojson from "../data/geojson.json";
 import MarkerClusterGroup from "react-leaflet-markercluster";
+import MarkerGeo from "./marker";
 
-class MyMap extends Component {
-    render() {
-        const position = [50.6446, 5.57341];
-        //const groupRef = useRef();
-        //const clusterRef = useRef();
-        // const createClusters = cluster => {
-        //     const childCount = cluster.getChildCount();
-        //     let size = "";
-        //     if (childCount < 10) {
-        //         size = "small";
-        //     } else if (childCount < 25) {
-        //         size = "medium";
-        //     } else {
-        //         size = "large";
-        //     }
-        //     return L.divIcon({
-        //         html: `<div><span><b>${childCount}</b></span></div>`,
-        //         className: `custom-marker-cluster custom-marker-cluster-${size}`,
-        //         iconSize: new L.point(40, 40),
-        //     });
-        // };
-        return (
-            <div>
-                <MapContainer center={position} zoom={13}>
-                    <TileLayer
-                        attribution={
-                            "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
-                        }
-                        url={
-                            "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        }
-                    />
-                    <FeatureGroup name={"Homes"}>
-                        <MarkerClusterGroup>
-                            <Marker position={[50.6441, 5.57338]} />
-                            <GeoJSON data={geojson} />
-                        </MarkerClusterGroup>
-                    </FeatureGroup>
-                </MapContainer>
-            </div>
-        );
-    }
-}
+const MyMap = () => {
+    const position = [50.6446, 5.57341];
+    const groupRef = useRef();
+    const clusterRef = useRef();
+
+    // const createPopups = (feature = {}, layer) => {
+    //     const { properties = {} } = feature
+    //     const { address, price, bedrooms, bathrooms } = properties
+    //     const popup = L.popup()
+    //     const html = `
+    //       <div class="popup-container">
+    //       </div>`
+    //       popup.setContent(html)
+    //       layer.bindPopup(popup)
+    //     }
+
+    return (
+        <div>
+            <MapContainer
+                style={{height: "100vh"}}
+                center={position}
+                zoom={17}
+                minZoom={12}
+                maxZoom={18}>
+                <TileLayer
+                    attribution={
+                        "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
+                    }
+                    url={"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}
+                />
+                <FeatureGroup ref={groupRef} name={"Homes"}>
+                    <Marker position={position} />
+                    <MarkerClusterGroup ref={clusterRef}>
+                        <MarkerGeo />
+                    </MarkerClusterGroup>
+                </FeatureGroup>
+            </MapContainer>
+        </div>
+    );
+};
 
 export default MyMap;
 ///api/datasets/1.0/search/?q=
