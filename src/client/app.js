02737dbba52f366099d4773073ed15c316211fa0
Maud-Pixel <leleuxmaud@gmail.com>
2021-02-18 09:16:59 +0100

    commit après merge

--- src/client/app.js
+++ src/client/app.js
@@ -10,8 +10,8 @@ import * as React from "react";
 import ReactDOM from "react-dom";
 import "./assets/style.css";
 
-//import HomeBackGround from "./components/homebackground";
-import MyMap from "./components/map/map";
+import HomeBackGround from "./components/home/homebackground";
+//import MyMap from "./components/map/map";
 
-//ReactDOM.render(<HomeBackGround />, document.querySelector("#bodyBackground"));
-ReactDOM.render(<MyMap />, document.querySelector("#main"));
+ReactDOM.render(<HomeBackGround />, document.querySelector("#bodyBackground"));
+//ReactDOM.render(<MyMap />, document.querySelector("#main"));
