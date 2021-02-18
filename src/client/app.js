72f27662be9c8511fdf68168fa71782bc28d5ff5
Marmouz82 <gtn.massart@gmail.com>
2021-02-18 15:51:24 +0100

    leader board

--- src/client/app.js
+++ src/client/app.js
@@ -8,10 +8,17 @@
 
 import * as React from "react";
 import ReactDOM from "react-dom";
-import "./assets/style.css";
+// import "./assets/style.css";
 
-import HomeBackGround from "./components/home/homebackground";
-//import MyMap from "./components/map/map";
+// import HomeBackGround from "./components/home/homebackground";
+// //import MyMap from "./components/map/map";
 
-ReactDOM.render(<HomeBackGround />, document.querySelector("#bodyBackground"));
-//ReactDOM.render(<MyMap />, document.querySelector("#main"));
+// ReactDOM.render(<HomeBackGround />, document.querySelector("#bodyBackground"));
+// //ReactDOM.render(<MyMap />, document.querySelector("#main"));
+
+// =====================================
+import "./assets/leaderboard.css";
+
+import LeaderBoard from "./components/leaderboard/leaderboard";
+
+ReactDOM.render(<LeaderBoard />, document.querySelector("#bodyBackground"));
--- src/client/components/home/homebackground.js
+++ src/client/components/home/homebackground.js
@@ -1,10 +1,12 @@
 import * as React from "react";
 import Picture from "./home";
 import DisplayMenu from "../layout/header";
+import LeaderBoard from "../leaderboard/leaderBoard";
 
 const HomeBackGround = () => (
     <div id={"body"}>
         <DisplayMenu />
+        <LeaderBoard />
         <Picture />
     </div>
 );
