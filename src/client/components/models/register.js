1b765734440cc532273fc2c0d38096109a032f29
Achouffe666 <austin@benard.be>
2021-02-22 16:41:47 +0100

    maj with everyone

--- src/client/components/models/register.js
+++ src/client/components/models/register.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
-const registerSchema = new mongoose.Schema({
-    firstname: {type: String, required: true},
-    lastname: {type: String, required: true},
-    password: {type: String, required: true},
-    email: {type: String, required: true, unique: true},
-    birthday: {type: Date, required: false},
+const RegisterSchema = new mongoose.Schema({
+    username: {type: String, unique: true},
+    firstname: {type: String},
+    lastname: {type: String},
+    password: {type: String},
+    email: {type: String, unique: true},
+    birthday: {type: Date},
+    treeCount: {type: Number, default: 3},
 });
 
-module.exports = mongoose.model("Register", registerSchema);
+module.exports = mongoose.model("Register", RegisterSchema);
