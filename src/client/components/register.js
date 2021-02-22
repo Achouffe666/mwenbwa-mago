81d3aed4e62c84e89fb0cafb953fea29a26747c9
Maud-Pixel <leleuxmaud@gmail.com>
2021-02-22 16:13:55 +0100

    form

--- src/client/components/register.js
+++ src/client/components/register.js
@@ -1,60 +1,145 @@
-import * as React from "react";
+import React, {Component} from "react";
 import Image from "../assets/images/user.svg";
+import axios from "axios";
 
-const Register_field = () => (
-    <div className={"main"}>
-        <div className={"picture"}>
-            <img src={Image} />
-        </div>
-        <form action={"/"} method={"POST"} className={"register"}>
-            <label htmlFor={"firstname"}>{"Firstname:"}</label>
-            <input
-                type={"text"}
-                id={"firstname"}
-                name={"firstname"}
-                placeholder={"Firstname"}
-                required
-            />
+export default class Register_field extends Component {
+    constructor(props) {
+        super(props);
 
-            <label htmlFor={"lastname"}>{"Lastname:"}</label>
-            <input
-                type={"text"}
-                id={"lastname"}
-                name={"lastname"}
-                placeholder={"Lastname"}
-                required
-            />
+        this.onChangeUserName = this.onChangeUserName.bind(this);
+        this.onChangeFirstName = this.onChangeFirstName.bind(this);
+        this.onChangeLastName = this.onChangeLastName.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        //this.onChangeBirthday= this.onChangeBirthday.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
 
-            <label htmlFor={"password"}>{"Password:"}</label>
-            <input
-                type={"password"}
-                id={"password"}
-                placeholder={"Password"}
-                required
-            />
+        this.state = {
+            username: "",
+            firstname: "",
+            lastname: "",
+            password: "",
+            email: "",
+            birthday: "",
+        };
+    }
 
-            <label htmlFor={"email"}>{"Email:"}</label>
-            <input
-                type={"email"}
-                id={"email"}
-                name={"email"}
-                placeholder={"Email"}
-                required
-            />
+    handleSubmit(e) {
+        e.preventDefault();
 
-            <label htmlFor={"birthday"}>{"Birthday:"}</label>
-            <input
-                type={"date"}
-                id={"birthday"}
-                name={"birthday"}
-                placeholder={"Birthday"}
-                required
-            />
-            <div className={"button"}>
-                <input type={"submit"} className={"submit"} value={"submit"} />
+        const userObject = {
+            username: this.state.username,
+            email: this.state.email,
+            firstname: this.state.firstname,
+            lastname: this.state.lastname,
+            password: this.state.password,
+        };
+
+        axios
+            .post("/register", userObject)
+            .then(res => {
+                console.log(res.data);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+        this.setState({
+            name: "",
+            email: "",
+            firstname: "",
+            lastname: "",
+            password: "",
+        });
+    }
+
+    render() {
+        return (
+            <div className={"main"}>
+                <div className={"picture"}>
+                    <img src={Image} />
+                </div>
+                <form onSubmit={this.handleSubmit} className={"register"}>
+                    <label htmlFor={"username"}>{"Username:"}</label>
+                    <input
+                        type={"text"}
+                        id={"username"}
+                        name={"username"}
+                        placeholder={"Username"}
+                        value={this.state.username}
+                        onChange={e =>
+                            this.setState({username: e.target.value})
+                        }
+                        required
+                    />
+                    <label htmlFor={"firstname"}>{"Firstname:"}</label>
+                    <input
+                        type={"text"}
+                        id={"firstname"}
+                        name={"firstname"}
+                        placeholder={"Firstname"}
+                        value={this.state.firstname}
+                        onChange={e =>
+                            this.setState({firstname: e.target.value})
+                        }
+                        required
+                    />
+
+                    <label htmlFor={"lastname"}>{"Lastname:"}</label>
+                    <input
+                        type={"text"}
+                        id={"lastname"}
+                        name={"lastname"}
+                        placeholder={"Lastname"}
+                        value={this.state.lastname}
+                        onChange={e =>
+                            this.setState({lastname: e.target.value})
+                        }
+                        required
+                    />
+
+                    <label htmlFor={"password"}>{"Password:"}</label>
+                    <input
+                        type={"password"}
+                        id={"password"}
+                        placeholder={"Password"}
+                        value={this.state.password}
+                        onChange={e =>
+                            this.setState({password: e.target.value})
+                        }
+                        required
+                    />
+
+                    <label htmlFor={"email"}>{"Email:"}</label>
+                    <input
+                        type={"email"}
+                        id={"email"}
+                        name={"email"}
+                        placeholder={"Email"}
+                        value={this.state.email}
+                        onChange={e =>
+                            this.setState({lastname: e.target.value})
+                        }
+                        required
+                    />
+
+                    <label htmlFor={"birthday"}>{"Birthday:"}</label>
+                    <input
+                        type={"date"}
+                        id={"birthday"}
+                        name={"birthday"}
+                        placeholder={"Birthday"}
+                        required
+                    />
+                    <div className={"button"}>
+                        <input
+                            type={"submit"}
+                            className={"submit"}
+                            value={"submit"}
+                        />
+                    </div>
+                </form>
             </div>
-        </form>
-    </div>
-);
-
-export default Register_field;
+        );
+    }
+}
--- src/server/models/register.js
+++ src/server/models/register.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
 
 const RegisterSchema = new mongoose.Schema({
+    username: {type: String, unique: true},
     firstname: {type: String},
     lastname: {type: String},
     password: {type: String},
     email: {type: String, unique: true},
     birthday: {type: Date},
+    treeCount: {type: Number, default: 3},
 });
 
 module.exports = mongoose.model("Register", RegisterSchema);
