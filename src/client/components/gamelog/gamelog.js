3abdca65ec61f043d8b63dbb9ced24c824d39227
Olivier <olivier.charlier.web@gmail.com>
2021-03-01 14:53:24 +0100

    what?

--- src/client/components/gamelog/gamelog.js
+++ src/client/components/gamelog/gamelog.js
@@ -3,78 +3,80 @@ import React from "react";
 const leaf_icon = "../../assets/images/leaf.png";
 
 const Game = () => (
-    <div className={"leaderBoard-main"}>
-        <div className={"leaderBoard-box"}>
-            <table>
-                <thead>
-                    <tr>
-                        <th className={"leaderBoard-title"} colSpan={"3"}>
-                            {"Game Log"}
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td className={"leaderBoard-subtitle"}>{"#"}</td>
-                        <td className={"leaderBoard-subtitle"}>{"User"}</td>
-                        <td>
-                            <img src={leaf_icon} width={"25px"} />
-                        </td>
-                        <div className={"line"} />
-                    </tr>
-                    <tr className={"leaderBoard-rows"}>
-                        <td>{"1"}</td>
-                        <td>{"Maud"}</td>
-                        <td>{"180"}</td>
-                    </tr>
-                    <tr className={"leaderBoard-rows"}>
-                        <td>{"2"}</td>
-                        <td />
-                        <td />
-                    </tr>
-                    <tr className={"leaderBoard-rows"}>
-                        <td>{"3"}</td>
-                        <td />
-                        <td />
-                    </tr>
-                    <tr className={"leaderBoard-rows"}>
-                        <td>{"4"}</td>
-                        <td />
-                        <td />
-                        <td />
-                    </tr>
-                    <tr className={"leaderBoard-rows"}>
-                        <td>{"5"}</td>
-                        <td />
-                        <td />
-                    </tr>
-                    <tr className={"leaderBoard-rows"}>
-                        <td>{"6"}</td>
-                        <td />
-                        <td />
-                    </tr>
-                    <tr className={"leaderBoard-rows"}>
-                        <td>{"7"}</td>
-                        <td />
-                        <td />
-                    </tr>
-                    <tr className={"leaderBoard-rows"}>
-                        <td>{"8"}</td>
-                        <td />
-                        <td />
-                    </tr>
-                    <tr className={"leaderBoard-rows"}>
-                        <td>{"9"}</td>
-                        <td />
-                        <td />
-                    </tr>
-                    <tr className={"leaderBoard-rows"}>
-                        <td>{"10"}</td>
-                        <td />
-                        <td />
-                    </tr>
-                </tbody>
-            </table>
+    <div className={"leaderBoard-full"}>
+        <div className={"leaderBoard-main"}>
+            <div className={"leaderBoard-box"}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th className={"leaderBoard-title"} colSpan={"3"}>
+                                {"Game Log"}
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className={"leaderBoard-subtitle"}>{"#"}</td>
+                            <td className={"leaderBoard-subtitle"}>{"User"}</td>
+                            <td>
+                                <img src={leaf_icon} width={"25px"} />
+                            </td>
+                            <div className={"line"} />
+                        </tr>
+                        <tr className={"leaderBoard-rows"}>
+                            <td>{"1"}</td>
+                            <td>{"Maud"}</td>
+                            <td>{"180"}</td>
+                        </tr>
+                        <tr className={"leaderBoard-rows"}>
+                            <td>{"2"}</td>
+                            <td />
+                            <td />
+                        </tr>
+                        <tr className={"leaderBoard-rows"}>
+                            <td>{"3"}</td>
+                            <td />
+                            <td />
+                        </tr>
+                        <tr className={"leaderBoard-rows"}>
+                            <td>{"4"}</td>
+                            <td />
+                            <td />
+                            <td />
+                        </tr>
+                        <tr className={"leaderBoard-rows"}>
+                            <td>{"5"}</td>
+                            <td />
+                            <td />
+                        </tr>
+                        <tr className={"leaderBoard-rows"}>
+                            <td>{"6"}</td>
+                            <td />
+                            <td />
+                        </tr>
+                        <tr className={"leaderBoard-rows"}>
+                            <td>{"7"}</td>
+                            <td />
+                            <td />
+                        </tr>
+                        <tr className={"leaderBoard-rows"}>
+                            <td>{"8"}</td>
+                            <td />
+                            <td />
+                        </tr>
+                        <tr className={"leaderBoard-rows"}>
+                            <td>{"9"}</td>
+                            <td />
+                            <td />
+                        </tr>
+                        <tr className={"leaderBoard-rows"}>
+                            <td>{"10"}</td>
+                            <td />
+                            <td />
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 );
--- src/client/components/register/register.js
+++ src/client/components/register/register.js
@@ -57,94 +57,98 @@ export default class Register_field extends Component {
 
     render() {
         return (
-            <div className={"register_field_main"}>
-                <div className={"register_field_picture"}>
-                    <img src={Image} />
-                </div>
-                <form
-                    onSubmit={this.handleSubmit}
-                    className={"register_field_form"}>
-                    <label htmlFor={"username"}>{"Username:"}</label>
-                    <input
-                        type={"text"}
-                        id={"username"}
-                        name={"username"}
-                        placeholder={"Username"}
-                        value={this.state.username}
-                        onChange={e =>
-                            this.setState({username: e.target.value})
-                        }
-                        required
-                    />
-                    <label htmlFor={"firstname"}>{"Firstname:"}</label>
-                    <input
-                        type={"text"}
-                        id={"firstname"}
-                        name={"firstname"}
-                        placeholder={"Firstname"}
-                        value={this.state.firstname}
-                        onChange={e =>
-                            this.setState({firstname: e.target.value})
-                        }
-                        required
-                    />
-
-                    <label htmlFor={"lastname"}>{"Lastname:"}</label>
-                    <input
-                        type={"text"}
-                        id={"lastname"}
-                        name={"lastname"}
-                        placeholder={"Lastname"}
-                        value={this.state.lastname}
-                        onChange={e =>
-                            this.setState({lastname: e.target.value})
-                        }
-                        required
-                    />
-
-                    <label htmlFor={"password"}>{"Password:"}</label>
-                    <input
-                        type={"password"}
-                        id={"password"}
-                        placeholder={"Password"}
-                        value={this.state.password}
-                        onChange={e =>
-                            this.setState({password: e.target.value})
-                        }
-                        required
-                    />
-
-                    <label htmlFor={"email"}>{"Email:"}</label>
-                    <input
-                        type={"email"}
-                        id={"email"}
-                        name={"email"}
-                        placeholder={"Email"}
-                        value={this.state.email}
-                        onChange={e => this.setState({email: e.target.value})}
-                        required
-                    />
-
-                    <label htmlFor={"birthday"}>{"Birthday:"}</label>
-                    <input
-                        type={"date"}
-                        id={"birthday"}
-                        name={"birthday"}
-                        placeholder={"Birthday"}
-                        required
-                    />
-                    <CirclePicker
-                        color={this.state.userColor}
-                        onChangeComplete={this.handleChangeComplete}
-                    />
-                    <div className={"button"}>
-                        <input
-                            type={"submit"}
-                            className={"register_field_submit"}
-                            value={"submit"}
-                        />
+            <div className={"leaderBoard-full"}>
+                <div className={"register_field_main"}>
+                    <div className={"register_field_picture"}>
+                        <img src={Image} />
                     </div>
-                </form>
+                    <form
+                        onSubmit={this.handleSubmit}
+                        className={"register_field_form"}>
+                        <label htmlFor={"username"}>{"Username:"}</label>
+                        <input
+                            type={"text"}
+                            id={"username"}
+                            name={"username"}
+                            placeholder={"Username"}
+                            value={this.state.username}
+                            onChange={e =>
+                                this.setState({username: e.target.value})
+                            }
+                            required
+                        />
+                        <label htmlFor={"firstname"}>{"Firstname:"}</label>
+                        <input
+                            type={"text"}
+                            id={"firstname"}
+                            name={"firstname"}
+                            placeholder={"Firstname"}
+                            value={this.state.firstname}
+                            onChange={e =>
+                                this.setState({firstname: e.target.value})
+                            }
+                            required
+                        />
+
+                        <label htmlFor={"lastname"}>{"Lastname:"}</label>
+                        <input
+                            type={"text"}
+                            id={"lastname"}
+                            name={"lastname"}
+                            placeholder={"Lastname"}
+                            value={this.state.lastname}
+                            onChange={e =>
+                                this.setState({lastname: e.target.value})
+                            }
+                            required
+                        />
+
+                        <label htmlFor={"password"}>{"Password:"}</label>
+                        <input
+                            type={"password"}
+                            id={"password"}
+                            placeholder={"Password"}
+                            value={this.state.password}
+                            onChange={e =>
+                                this.setState({password: e.target.value})
+                            }
+                            required
+                        />
+
+                        <label htmlFor={"email"}>{"Email:"}</label>
+                        <input
+                            type={"email"}
+                            id={"email"}
+                            name={"email"}
+                            placeholder={"Email"}
+                            value={this.state.email}
+                            onChange={e =>
+                                this.setState({email: e.target.value})
+                            }
+                            required
+                        />
+
+                        <label htmlFor={"birthday"}>{"Birthday:"}</label>
+                        <input
+                            type={"date"}
+                            id={"birthday"}
+                            name={"birthday"}
+                            placeholder={"Birthday"}
+                            required
+                        />
+                        <CirclePicker
+                            color={this.state.userColor}
+                            onChangeComplete={this.handleChangeComplete}
+                        />
+                        <div className={"button"}>
+                            <input
+                                type={"submit"}
+                                className={"register_field_submit"}
+                                value={"submit"}
+                            />
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
